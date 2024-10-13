e11bbeebdae3257a4010994103ca93daee0ab3fe
techymualim <syedhassam2001@gmail.com>
2024-10-13 13:11:26 +0500

    feat:docker settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -328,3 +328,5 @@ SPECTACULAR_SETTINGS = {
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=['http://localhost:8000', 'http://127.0.0.1:8000'])
--- config/settings/local.py
+++ config/settings/local.py
@@ -14,7 +14,7 @@ SECRET_KEY = env(
     default="i9bAB2P9AVfcUJOsqEN5jC5RVLam3zZmcNOt4IF36U3uCRuCyJhSFzcp2YqecEld",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+
 
 # CACHES
 # ------------------------------------------------------------------------------

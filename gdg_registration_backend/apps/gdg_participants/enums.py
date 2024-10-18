189aa58cf0c3e57b99ae70b97add82ff15054e41
techymualim <syedhassam2001@gmail.com>
2024-10-19 00:58:13 +0500

    modify: added enum

--- gdg_registration_backend/apps/gdg_participants/enums.py
+++ gdg_registration_backend/apps/gdg_participants/enums.py
@@ -2,24 +2,25 @@ from enum import Enum
 
 
 class ParticipantType(Enum):
-    PROFESSIONAL = 'PROFESSIONAL'
+    PROFESSIONAL = "PROFESSIONAL"
     STUDENT = "STUDENT"
-    
+
 
 class ParticipantStatus(Enum):
-    PENDING = 'PENDING'
-    SHORTLISTED = 'SHORTLISTED'
-    CONFIRMED = 'CONFIRMED'
-    ATTENDED = 'ATTENDED'
+    PENDING = "PENDING"
+    SHORTLISTED = "SHORTLISTED"
+    CONFIRMED = "CONFIRMED"
+    REJECTED = "REJECTED"
+    ATTENDED = "ATTENDED"
 
     @classmethod
     def is_valid_status(cls, status):
         """
         Check if the provided status is a valid enum value.
-        
+
         Args:
             status (str): The status to validate.
-        
+
         Returns:
             bool: True if the status is valid, False otherwise.
         """
